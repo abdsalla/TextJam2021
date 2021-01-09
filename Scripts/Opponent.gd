25ddda11f9ce093533da16ba1f06f3d0fb902fbf
Brandon Michelsen <brandongmichelsen@gmail.com>
2021-01-08 20:54:13 -0800

    Fixed the dictionaries (swapped number and dialogue)

--- Scripts/Opponent.gd
+++ Scripts/Opponent.gd
@@ -7,17 +7,17 @@ signal insult_score(score)			# Signal for the score of an insult
 
 # Dialogue dictionaries
 var insults = {
-	0: "Insult 1",
-	1: "Insult 2", 
-	2: "Insult 3",
-	3: "Insult 4"
+	"Insult": 0,
+	"Insult 2": 1, 
+	"Insult 3": 1,
+	"Insult 4": 2
 	}
 
 var comebacks = {
-	0: "Comeback 1",
-	1: "Comeback 2",
-	2: "Comeback 3",
-	3: "Comeback 4"
+	"Comeback 1": 2,
+	"Comeback 2": 1,
+	"Comeback 3": 3,
+	"Comeback 4": 0
 	}
 
 
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,17 +4,17 @@ signal write_pl_txt(txt)
 
 # Dialogue dictionaries
 var insults = {
-	0: "Insult 1",
-	1: "Insult 2", 
-	2: "Insult 3",
-	3: "Insult 4"
+	"Insult 1": 0,
+	"Insult 2": 1,
+	"Insult 3": 2,
+	"Insult 4": 2
 	}
 
 var comebacks = {
-	0: "Comeback 1",
-	1: "Comeback 2",
-	2: "Comeback 3",
-	3: "Comeback 4"
+	"Comeback 1": 1,
+	"Comeback 2": 0,
+	"Comeback 3": 1,
+	"Comeback 4": 2
 	}
 	
 # Called at the start of the game (MainHandler's _ready function)
